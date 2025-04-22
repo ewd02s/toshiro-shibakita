c65d31f2224b3f3d82df9090a3413414eaa63727
Eder William ॐ <ewd02s@gmail.com>
2025-04-22 19:20:03 -0300

    Add files via upload
    Desafio de projeto de Docker: Utilização Prática no Cenário de Microsserviços

---
+++ Docker/banco.sql
@@ -0,0 +1,9 @@
+CREATE TABLE dados (
+    ProdutoID int,
+    Nome varchar(50),
+    Marca varchar(50),
+    Quantidade varchar(50),
+    Preco varchar(10),
+    Valor varchar(10),
+    Host varchar(50)
+);
